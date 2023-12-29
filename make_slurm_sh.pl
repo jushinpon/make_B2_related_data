053d50451dfd960b74ed753e3517f564d22e82d3
jushinpon <jushinpon@gmail.com>
2023-12-29 23:07:42 +0800

    modify scripts

--- make_slurm_sh.pl
+++ make_slurm_sh.pl
@@ -16,12 +16,12 @@ use List::Util qw/shuffle/;
 my $filefold = "QEall_set";
 my $submitJobs = "no";
 my %sbatch_para = (
-            nodes => 2,#how many nodes for your lmp job
+            nodes => 1,#how many nodes for your lmp job
             #nodes => 1,#how many nodes for your lmp job
             threads => 1,,#modify it to 2, 4 if oom problem appears
             #cpus_per_task => 1,
-            partition => "C16M32",#which partition you want to use
-            #partition => "All",#which partition you want to use
+            #partition => "C16M32",#which partition you want to use
+            partition => "All",#which partition you want to use
             runPath => "/opt/thermoPW-7-2/bin/pw.x",          
             );
 
